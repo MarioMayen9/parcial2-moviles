7d7d5a4d91b63f70b2a6497d60528ccf33943793
Mario E Mayen <00220618@uca.edu.sv>
2023-05-23 15:25:20 -0600

    agregando capa modelo

---
+++ parcial2moviles/app/src/main/java/com/mayencastro/parcial2moviles/data/DummyData.kt
@@ -0,0 +1,18 @@
+package com.mayencastro.parcial2moviles.data
+
+import com.mayencastro.parcial2moviles.data.model.Sport
+
+val name = "Ernesto"
+val rules = "Gamer"
+
+
+//val name2 = "Mario"
+//val descripcion2 = "Gamer"
+
+
+
+var model = mutableListOf(
+    Sport(name, rules)//, category, description, qualification),
+    // Model(name2, descripcion2) //, category2, description2, qualification2),
+
+)
---
+++ parcial2moviles/app/src/main/java/com/mayencastro/parcial2moviles/data/model/Sport.kt
@@ -0,0 +1,9 @@
+package com.mayencastro.parcial2moviles.data.model
+
+data class Sport (
+    val name: String,
+    val rules: String
+
+
+
+)
